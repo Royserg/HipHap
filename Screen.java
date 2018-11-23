e88f8cf64d0bd2619a9514f70e97b174c56e5c77
Royserg <staniojakub@gmail.com>
2018-11-23 19:46:15 +0100

    Add showLogin - ask for credentials

--- Screen.java
+++ Screen.java
@@ -1,5 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Screen {
+
+    private static Scanner scn = new Scanner(System.in);
+
+    /**
+     * Showing initial login screen when application run
+     */
+    public static void showLogin() {
+        String username;
+        String password;
+
+        System.out.println("HipHapOrg");
+        System.out.println("======= Login =======");
+        System.out.println();
+
+        System.out.print("Username: ");
+        username = scn.nextLine();
+
+        System.out.print("Password: ");
+        password = scn.nextLine();
+
+        //TODO: implement validating info here or in Main
+    }
 
 }
