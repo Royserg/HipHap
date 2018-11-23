d8ced2dfa677f5a22fc13b1aa55e2ea6a9faa640
Cosmin <cosminscolan@gmail.com>
2018-11-23 12:02:29 +0100

    Initial file - test

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    ArrayList<Event> events = new Event();
+    ArrayList<Employee> employees = new Employee();
+    ArrayList<Partner> partners = new Partner();
+    ArrayList<Customer> customers = new Customer();
+
+    public static void main(String[] args) {
+    String name;
+
+    }
+
+    public void initiateData() {
+
+    }
+}
