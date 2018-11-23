eaf8f151141410a9fab76bd5ab32fa8552ef8d08
Royserg <staniojakub@gmail.com>
2018-11-23 22:12:56 +0100

    Replace setters with constructor and add getters

--- Employee.java
+++ Employee.java
@@ -1,32 +1,51 @@
 package com.company;
 
 public class Employee {
-    int ID;
-    String name;
-    String password;
-    String email;
+    private int ID;
+    private String name;
+    private String password;
+    private String email;
 
     // Constructor
-    Employee() {
-    }
-
-    // Set-ers
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public void setName(String name) {
+    public Employee(int id, String name, String pass, String email) {
+        this.ID = id;
         this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
+        this.password = pass;
         this.email = email;
     }
 
-    // Get-ers
-    public
+    // Getters
+
+    /**
+     * get Employee id
+     * @return id (int) of Employee
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * get Employee name
+     * @return name (String) of Employee
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * get Employee password
+     * @return password (String) of Employee
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * get Employee email address
+     * @return email (String) of Employee
+     */
+    public String getEmail() {
+        return email;
+    }
+
 }
