9259f65513f613ebbc01e82b57d1b5ebd1312048
Royserg <staniojakub@gmail.com>
2018-11-23 19:34:13 +0100

    Initial commit

---
+++ Employee.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Employee {
+    int ID;
+    String name;
+    String password;
+    String email;
+
+    // Constructor
+    Employee() {
+    }
+
+    // Set-ers
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Get-ers
+//    public
+}
---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+//    ArrayList<Event> events;
+//    ArrayList<Employee> employees;
+//    ArrayList<Partner> partners;
+//    ArrayList<Customer> customers;
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+
+    public void initiateData() {
+
+    }
+}
