ddc948b22d80d7a3911fa6f7b06fd6ed8fbd092f
eaglett <35506883+eaglett@users.noreply.github.com>
2018-11-27 23:37:16 +0100

    Modified Screen.Login() and created Login validation Main.validateLogin()

--- Main.java
+++ Main.java
@@ -12,12 +12,39 @@ public class Main {
     static ArrayList<Partner> partners = new ArrayList<Partner>();
     static ArrayList<Customer> customers = new ArrayList<Customer>();
 
+    //Screen screen = new Screen(); dont we have to create an instance because of username and pass fields? and then call showLogin() on the instance
+
     public static void main(String[] args) {
 
-        // show login screen
-//        Screen.showLogin();
+        Screen.showLogin();
+        if( validateLogin() ){
+            //everything after login
+        }else{
+            System.out.print("Incorrect username or password. Try again.");
+            Screen.showLogin(); //login again
+        }
 
-        initiateData();
+        initiateData();//this goes inside if? or before login screen? I think it would be better to put it before login -Aneja
+    }
+
+    /*
+    * validates Login information to see if the user exists and if he entered the correct password
+    * */
+    private static boolean validateLogin(){
+        boolean validation = false;
+        int i=0;
+        boolean found = false;
+
+        while( !found ) {
+            if (employees.get(i).getName().equals(Screen.getUsername()))
+                found = true;
+            else i++;
+        }
+
+        if (employees.get(i).getPassword().equals(Screen.getPassword()))
+            validation = true;
+
+        return validation;
     }
 
     private static void initiateData() {
