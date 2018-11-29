89b675332bb1b4aed926b53ce3230616bc09fefd
Royserg <staniojakub@gmail.com>
2018-11-29 01:04:35 +0100

    Add file containing data and interactions

---
+++ Database.java
@@ -0,0 +1,19 @@
+package src;
+
+import src.events.Event;
+import src.users.Customer;
+import src.users.Employee;
+import src.users.Partner;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    private ArrayList<Event> events = FileManager.readEventsFile();
+    private ArrayList<Employee> employees = FileManager.readEmployeesFile();
+    private ArrayList<Partner> partners = FileManager.readPartnersFile();
+    private ArrayList<Customer> customers = FileManager.readCustomersFile();
+
+    // constructor
+    public Database() {};
+}
