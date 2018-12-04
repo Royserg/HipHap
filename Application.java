adbd6a88d79fb4762b3f92a140c5348006dfcde7
eaglett <35506883+eaglett@users.noreply.github.com>
2018-12-04 19:26:49 +0100

    editEvent finished

--- Application.java
+++ Application.java
@@ -478,6 +478,7 @@ public class Application {
         }
 
         //adding partners
+        //TODO: make sure this part adds a correct ID
         System.out.println("If the partners are needed for the event select the partners. If they are not needed, select 0");
         int currentPartner = 1;
         ArrayList<Integer> allPartners = new ArrayList<>();
@@ -486,7 +487,8 @@ public class Application {
             for(int i = 0; i < db.partners.size(); i++)
                 System.out.println(i+1 + " - " + db.partners.get(i).getName());
             currentPartner = Helper.selectOption(db.partners.size() + 1);
-            allPartners.add(currentPartner);
+            if(currentPartner != 0)
+                allPartners.add(db.partners.get(currentPartner-1).getID());
         }
 
         //assigning employee
@@ -575,16 +577,20 @@ public class Application {
                 }
                 int partnerSelected = Helper.selectOption(partnerIDs.size()+1);
 
+                //adding a partner
                 if(partnerSelected == 0){
-                    
+                    for(int i = 0; i < db.partners.size(); i++)
+                        System.out.println(i+1 + " - " + db.partners.get(i).getName());
+                    int newPartner = Helper.selectOption(db.partners.size() + 1);
+                    partnerIDs.add(db.partners.get(newPartner-1).getID());
+                }else{ //deleting a partner
+                    for(int i = 0; i < db.partners.size(); i++)
+                        System.out.println(i+1 + " - " + db.partners.get(i).getName());
+                    int delPartner = Helper.selectOption(1, db.partners.size());
+                    partnerIDs.remove(delPartner-1);
                 }
             }
         }
-
-
-
-
-
     }
 
     public void deleteEvent(int ID) {
