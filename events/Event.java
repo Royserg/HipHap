ac2820f4bb5152d961fda6323ec96bb226c33087
Jakub <staniojakub@gmail.com>
2018-12-04 23:13:42 +0100

    Add toString method

--- events/Event.java
+++ events/Event.java
@@ -227,4 +227,8 @@ public class Event {
         specs.add(spec);
     }
 
+    @Override
+    public String toString() {
+        return this.getName() + " - " + this.getEventType() + " - " + this.getServiceType();
+    }
 }
